a37c51eab91c716dcfe764a3c212084bd06f9caa
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-12-05 22:42:31 +0300

    чек стайл

--- src/main/java/com/javaacademy/car_avito/util/RequestParameterUtil.java
+++ src/main/java/com/javaacademy/car_avito/util/RequestParameterUtil.java
@@ -13,7 +13,7 @@ public class RequestParameterUtil {
         for (Map.Entry<String, String> entry : parameters.entrySet()) {
             parametersAsString.append(entry.getKey()).append("=").append(entry.getValue()).append("&");
         }
-        parametersAsString.deleteCharAt(parametersAsString.length() -1);
+        parametersAsString.deleteCharAt(parametersAsString.length() - 1);
         return parametersAsString.toString();
     }
 }
